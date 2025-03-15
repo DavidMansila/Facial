ac73ebc9fb37fb0da98f6ab7aaf32d7d34d54a5e
davidmansi <davidjosemsc@gmail.com>
2025-03-15 05:20:26 -0400

    Arreglos

--- routes/faceDetectionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const faceDetectionController = require('../controllers/faceDetectionController');
-
-// Ruta para la detección de rostros
-router.post('/detect-faces', faceDetectionController.detectFaces);
-
-module.exports = router;
